586c09ec828dcf1514885203edc7c47a37122bf2
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-01 10:29:22 -0500

    changed poll command to use embed

--- src/plugins/poll.js
+++ src/plugins/poll.js
@@ -8,7 +8,16 @@ module.exports.command = {
   },
   run: function (message, user, args) {
     let poll = args.slice(1, args.length).join(" ");
-    message.channel.send(poll).then((messageReaction) => {
+    const embed = new Discord.MessageEmbed()
+      .setTitle(poll)
+      .setThumbnail(
+        "https://www.clipartkey.com/mpngs/m/100-1003892_thumbs-up-down-thumbs-up-and-down-png.png"
+      )
+      .setDescription(
+        "React with a thumbs up or thumbs down emoji to vote on the poll."
+      )
+      .setColor("RANDOM");
+    message.channel.send({ embed: embed }).then((messageReaction) => {
       messageReaction.react("👍");
       messageReaction.react("👎");
     });
